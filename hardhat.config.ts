88d33763971c79e9a81e74b04da9a881ee356c01
Rahul Sethuram <rahul@sethuram.com>
2021-02-18 21:28:17 +0100

    Hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,8 +10,8 @@ import "hardhat-deploy-ethers";
 import "hardhat-deploy";
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
-const RINKEBY_PRIVATE_KEY =
-  process.env.RINKEBY_PRIVATE_KEY! ||
+const PRIVATE_KEY =
+  process.env.PRIVATE_KEY! ||
   "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"; // well known private key
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
@@ -50,7 +50,19 @@ const config: HardhatUserConfig = {
     },
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [RINKEBY_PRIVATE_KEY],
+      accounts: [PRIVATE_KEY],
+    },
+    bsc: {
+      url: "https://bsc-dataseed.binance.org/",
+      accounts: [PRIVATE_KEY],
+    },
+    xdai: {
+      url: "https://rpc.xdaichain.com/",
+      accounts: [PRIVATE_KEY],
+    },
+    matic: {
+      url: "https://rpc-mainnet.matic.network",
+      accounts: [PRIVATE_KEY],
     },
     coverage: {
       url: "http://127.0.0.1:8555", // Coverage launches its own ganache-cli client
